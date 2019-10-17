cfe5f130d47b8ce36c573294d40758d89cac7c12
fantomart <fantomart@mail.ru>
2019-10-17 12:08:06 +0300

    Update

--- parserURL/views.py
+++ parserURL/views.py
@@ -1,15 +1,11 @@
 from django.shortcuts import render
 from .models import Task
 
-def get_all_done_tasks():
-    return Task.objects.filter(is_done=True)
-
 def home(request):
 
     return render(
         request,
         'home.html',
-        context={'task_list':get_all_done_tasks(),
-                'results_list':get_all_done_tasks()},
+        context={'task_list':Task.objects.filter(is_done=True)},
     )
 
